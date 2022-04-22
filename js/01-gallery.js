cd8eb4d31c072566275cfac7f217e66c43b749bf
Artem Bystritskyi <artemb0607@gmail.com>
2022-04-22 20:16:21 +0300

    gallery/logic/add modal basiclightbox

--- js/01-gallery.js
+++ js/01-gallery.js
@@ -1,4 +1,44 @@
 import { galleryItems } from './gallery-items.js';
 // Change code below this line
 
-console.log(galleryItems);
+const galleryContainer = document.querySelector(".gallery");
+
+function makeGalleryPhotosMarkup(items) {
+    return items.map(({ preview, original, description }) => {
+        return `
+        <a class="gallery__link" href="#">
+            <img
+            class="gallery__image"
+            src="${preview}"
+            data-source="${original}"
+            alt="${description}"
+            />
+        </a>`
+    }).join("");
+}
+
+galleryContainer.insertAdjacentHTML("afterbegin", makeGalleryPhotosMarkup(galleryItems));
+
+galleryContainer.addEventListener("click", onPhotoClick);
+
+function onPhotoClick(evt) {
+    evt.preventDefault();
+
+    if (evt.target.nodeName !== "IMG") {
+        return
+    }
+
+    const containerImgEl = document.querySelector(".gallery__image");
+
+    const instance = basicLightbox.create(`<img src="${containerImgEl.dataset.source}"/>`, {
+        onShow: instance => {
+            window.addEventListener("keydown", evt => {
+                if (evt.code === "Escape") {
+                    instance.close();
+                }
+            })
+        }
+    });
+
+    instance.show();
+}
