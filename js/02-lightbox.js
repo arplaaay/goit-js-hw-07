3c1ff6bb4a2b649b2f003b9e39b954f7a1a21fb3
Artem Bystritskyi <artemb0607@gmail.com>
2022-04-22 20:17:21 +0300

    gallery/logic/add modal swipe simplelightbox

--- js/02-lightbox.js
+++ js/02-lightbox.js
@@ -1,4 +1,30 @@
 import { galleryItems } from './gallery-items.js';
 // Change code below this line
 
-console.log(galleryItems);
+const galleryContainer = document.querySelector(".gallery");
+
+function makeGalleryPhotosMarkup(items) {
+    return items.map(({ preview, original, description }) => {
+        return `
+        <a class="gallery__item" href="${original}">
+            <img class="gallery__image" src="${preview}" title="${description}" />
+        </a>`
+    }).join("");
+}
+
+galleryContainer.insertAdjacentHTML("afterbegin", makeGalleryPhotosMarkup(galleryItems));
+
+galleryContainer.addEventListener("click", onPhotoClick);
+
+function onPhotoClick(evt) {
+    evt.preventDefault();
+
+    if (evt.target.nodeName !== "IMG") {
+        return;
+    }
+
+    const lightbox = new SimpleLightbox(".gallery a", {
+        captionDelay: 250
+    });
+    
+}
